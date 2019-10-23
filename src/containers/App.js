ae7169d9391c16ab6cc60b35c414f2126c5dafd2
Vinicius A L Souza <vinicius.060200@gmail.com>
2019-10-23 17:35:01 -0400

    Remove reset stylesheet

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,17 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import Navbar from "../components/Navbar";
 
 class App extends Component{
 
     render() {
 
-        return(<Navbar/>);
+        return(
+        <Fragment>
+            <CssBaseline />
+            <Navbar/>
+        </Fragment>);
     }
 };
 
