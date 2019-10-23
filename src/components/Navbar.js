073f9d64739ed53911b0c873bdffc50965064fb0
Vinicius A L Souza <vinicius.060200@gmail.com>
2019-10-23 12:52:42 -0400

    Create navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React, { Fragment, Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+class Navbar extends Component {
+
+    render(){
+        return (
+            <Fragment>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton
+                            edge="start" color="inherit" aria-label="menu"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                    </Toolbar>
+                </AppBar>
+            </Fragment>
+        );
+    }
+}
+
+export default Navbar;
