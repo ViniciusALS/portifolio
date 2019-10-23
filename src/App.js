c652dd19e17fa70ac637503a668a89bdd3de195c
Vinicius A L Souza <vinicius.060200@gmail.com>
2019-10-23 15:17:24 -0400

    Create App component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Types from "./Types";
+
+class App extends Component{
+
+    render() {
+
+        return(<div>sd</div>);
+    }
+};
+
+export default App;
