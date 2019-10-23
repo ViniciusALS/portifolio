9ddb09f3f26db76da795f9a255b76c5fffc939d2
Vinicius A L Souza <vinicius.060200@gmail.com>
2019-10-23 17:35:42 -0400

    implement navbar using material ui components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,74 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment } from 'react';
+import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
-class Navbar extends Component {
+class Navbar extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            open: false,
+        }
+    };
+
+    handleClick(event) {
+
+        event.preventDefault();
+
+        this.setState({
+            open: !this.state.open
+        });
+    };
 
     render(){
+
         return (
             <Fragment>
-                <AppBar position="static">
+                <AppBar position="fixed">
                     <Toolbar>
                         <IconButton
-                        edge="start" color="inherit" aria-label="menu" >
+                            color="inherit"
+                            aria-label="open menu"
+                            onClick={this.handleClick}
+                            edge="start">
 
                             <MenuIcon />
                         </IconButton>
                     </Toolbar>
                 </AppBar>
+                <Drawer
+                    variant="persistent"
+                    anchor="left"
+                    open={this.state.open}>
+
+                    <div>
+                        <IconButton onClick={this.handleClick}>
+                            <ChevronLeftIcon />
+                        </IconButton>
+                    </div>
+                    <List>
+                        <ListItem button>
+                            <ListItemText primary="Home" />
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemText primary="Resume" />
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemText primary="Projects" />
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemText primary="Contact" />
+                        </ListItem>
+                    </List>
+                </Drawer>
             </Fragment>
         );
     }
