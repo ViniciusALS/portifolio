e68c79152cb0db4b01471cab74de05aaf3c9ba65
Vinicius A L Souza <vinicius.060200@gmail.com>
2019-10-28 15:22:05 -0400

    Create styled component for Tabs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import CloseIcon from '@material-ui/icons/Close';
 import { withStyles } from '@material-ui/core/styles';
-import StyledTabs from '../styled components/StyledTabs';
-import Tab from '@material-ui/core/Tab';
+import StyledTabs from '../styledComponents/StyledTabs';
 
 class Navbar extends React.Component {
 
--- src/styledComponents/StyledTabs.js
+++ src/styledComponents/StyledTabs.js
@@ -2,6 +2,9 @@ import {withStyles} from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 
 const StyledTabs = withStyles({
+    root: {
+        paddingTop: '5em',
+    },
     indicator: {
         backgroundColor: '#F2F2F2',
         width: '5px',
