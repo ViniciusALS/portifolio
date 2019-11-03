bd30fc5bbacd4f9d88748c84a192e074a714dab8
Vinicius A L Souza <vinicius.060200@gmail.com>
2019-11-03 03:24:02 -0500

    Create Dropbox component

---
+++ src/components/Dropbox.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import InputBase from '@material-ui/core/InputBase';
+
+const BootstrapInput = withStyles(theme => ({
+    root: {
+        borderRadius: 4,
+        backgroundColor: '#2A6AA5',
+        color: '#F2F2F2',
+        textAlign: 'center',
+    },
+    input: {
+        padding: '12px 0',
+        '&:focus': {
+            borderRadius: 4,
+            boxShadow: '0 0 0 0.2rem rgba(255, 255, 255, .25)',
+        },
+    },
+}))(InputBase);
+
+const BootstrapSelect = withStyles(theme => ({
+    root: {
+        borderRadius: 4,
+        backgroundColor: '#2A6AA5',
+        color: '#F2F2F2',
+        textAlign: 'center',
+    },
+    icon: {
+        color: '#F2F2F2',
+        margin: '0 0.5rem',
+    }
+}))(Select);
+
+export default function(props) {
+    const [skill, setSkill] = React.useState(0);
+
+    const handleChange = event => {
+        setSkill(event.target.value);
+    };
+
+    return (
+        <BootstrapSelect
+            value={skill}
+            onChange={handleChange}
+            input={<BootstrapInput />}>
+
+            {props.options.map((text, index)=>(
+                <MenuItem key={text} value={index}>{text}</MenuItem>
+            ))}
+        </BootstrapSelect>
+    );
+}
